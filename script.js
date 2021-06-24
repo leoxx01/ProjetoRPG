67a2d9ef008cadc250ab994c27cb37241276f987
leo <leocarminhoto@gmail.com>
2021-06-24 13:32:19 -0300

    Add Chat

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 
 document.querySelector('.person').style.marginLeft = '250px'
-document.addEventListener('keypress', teste)
+document.addEventListener('keypress', keys)
 
- function teste(e){
+ function keys(e){
  	console.log(e.which)
   if(e.which === 97){
   	var a = parseInt(document.querySelector('.person').style.marginLeft)
     document.querySelector('.person').style.marginLeft = `${a-250}px`
   }
-  if(e.which === 100){
+  else if(e.which === 100){
   	var a = parseInt(document.querySelector('.person').style.marginLeft)
     document.querySelector('.person').style.marginLeft = `${a+250}px`
   }
+  else if(e.which === 13){
+    checkCampoTextChat()
+    console.log('aa')
+  }
  }
+ function checkCampoTextChat(){
+	if(document.querySelector('.campo').value != '' ){
+    console.log('a')
+    document.querySelector('.chat').innerHTML= `${document.querySelector('.chat').innerHTML}<br>${document.querySelector('.campo').value}`
+    document.querySelector('.campo').value = ''
+  }
+}
+
